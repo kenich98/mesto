427d465a2cc7a5cb47fab0c8755ac32e646e6679
777 <kenich98@yadnex.ru>
2023-07-19 22:11:58 +0700

    Исправил замечания 1 итерации, исправил js

--- scripts/index.js
+++ scripts/index.js
@@ -2,19 +2,6 @@ const popup = document.querySelector('.popup');
 const popupOpenButton = document.querySelector('.profile__edit-button');
 const popupCloseButton = popup.querySelector('.popup__close-button');
 
-const popupToggle = function () {
-    popup.classList.toggle('popup_opened');
-}
-popupOpenButton.addEventListener("click", popupToggle);
-
-popupCloseButton.addEventListener("click", popupToggle);
-
-const closePopupByClickingOnOverlay = function (event) {
-    if (event.target == event.currentTarget) {
-        popupToggle();
-    }
-};
-
 let formElement = document.querySelector('.popup__container');
 let closeButton = document.querySelector('.popup__save-button');
 
@@ -23,17 +10,24 @@ let jobProfile = document.querySelector('.profile__subtitle');
 let nameInput = popup.querySelector('.popup__input_name');
 let jobInput = popup.querySelector('.popup__input_job')
 
+const popupToggle = function () {
+    popup.classList.toggle('popup_opened');
+    let profileTitle = document.querySelector('.profile__title').textContent;
+    document.querySelector('.popup__input_name').value = profileTitle;
+    let profileSubtitle = document.querySelector('.profile__subtitle').textContent;
+    document.querySelector('.popup__input_job').value = profileSubtitle;
+}
 
 function handleFormSubmit(evt) {
     evt.preventDefault();
-    nameInput.value;
-    jobInput.value;
     nameProfile.textContent = nameInput.value;
     jobProfile.textContent = jobInput.value;
 
-    popupCloseButton.addEventListener('click', function () {
-        popup.classList.remove('popup_opened');
-    });
+    popup.classList.remove('popup_opened');
 }
 
-formElement.addEventListener('submit', handleFormSubmit); 
+formElement.addEventListener('submit', handleFormSubmit);
+
+popupOpenButton.addEventListener("click", popupToggle);
+
+popupCloseButton.addEventListener("click", popupToggle);
