4c745bc84c96cea1406a7e45b9950f5b8b91d288
777 <kenich98@yadnex.ru>
2023-07-29 17:07:17 +0700

    поправил замечания, переделал js

--- scripts/index.js
+++ scripts/index.js
@@ -2,32 +2,32 @@ const popup = document.querySelector('.popup');
 const popupOpenButton = document.querySelector('.profile__edit-button');
 const popupCloseButton = popup.querySelector('.popup__close-button');
 
-let formElement = document.querySelector('.popup__container');
-let closeButton = document.querySelector('.popup__save-button');
+let formElement = document.querySelector('.popup__info')
+let cardName = document.querySelector('.profile__title');
+let cardJob = document.querySelector('.profile__subtitle');
+let nameInput = popup.querySelector('.popup__input_info_name');
+let jobInput = popup.querySelector('.popup__input_info_job');
 
-let nameProfile = document.querySelector('.profile__title');
-let jobProfile = document.querySelector('.profile__subtitle');
-let nameInput = popup.querySelector('.popup__input_name');
-let jobInput = popup.querySelector('.popup__input_job')
+const closePopup = function () {
+    popup.classList.remove('popup_opened');
+}
 
-const popupToggle = function () {
-    popup.classList.toggle('popup_opened');
-    let profileTitle = document.querySelector('.profile__title').textContent;
-    document.querySelector('.popup__input_name').value = profileTitle;
-    let profileSubtitle = document.querySelector('.profile__subtitle').textContent;
-    document.querySelector('.popup__input_job').value = profileSubtitle;
+const popupAdd = function () {
+    popup.classList.add('popup_opened');
+    nameInput.value = cardName.textContent;
+    jobInput.value = cardJob.textContent;
 }
 
 function handleFormSubmit(evt) {
     evt.preventDefault();
-    nameProfile.textContent = nameInput.value;
-    jobProfile.textContent = jobInput.value;
-
-    popup.classList.remove('popup_opened');
+    let nameValue = nameInput.value;
+    let jobValue = jobInput.value;
+    cardName.textContent = nameValue;
+    cardJob.textContent = jobValue;
+    closePopup();
 }
 
+
 formElement.addEventListener('submit', handleFormSubmit);
-
-popupOpenButton.addEventListener("click", popupToggle);
-
-popupCloseButton.addEventListener("click", popupToggle);
+popupOpenButton.addEventListener('click', popupAdd);
+popupCloseButton.addEventListener('click', closePopup);
